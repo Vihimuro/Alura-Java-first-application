2ef0e1df405ab4687da9d0fd34ff0fff734fb01d
vihimuro <vihi1208@gmail.com>
2023-10-06 08:40:28 -0400

    Lesson 02 - Building the movie catalog

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,21 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("I concluded the 01 class and now I'm diving into Java!");
+        System.out.println("That's the Screen Match");
+        System.out.println("Movie: Top Gun: Maverick");
+
+        int releaseYear = 2022;
+        boolean isIncludedInPlan = true;
+
+        double average = (9.8 + 6.3 + 8.0)/3;
+        System.out.printf("Average: %.2f%n", average);
+        String synopsis = """
+                    Top Gun Movie
+                    Adventure Movie with 80's heartthrob
+                    Very Good!
+                    Release Year:""" + " " + releaseYear;
+        System.out.println(synopsis);
+
+        int classification = (int) (average/2);
+        System.out.println("Classification: " +classification + "/5");
     }
 }
